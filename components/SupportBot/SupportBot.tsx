feeef21528243d8bac6d7cc8ce8a6edb5af95c13
DiegoGalma <diegogalmarini@gmail.com>
2026-01-09 20:52:17 +0100

    fix: remove green notification badge from chat button

--- components/SupportBot/SupportBot.tsx
+++ components/SupportBot/SupportBot.tsx
@@ -172,12 +172,6 @@ export const SupportBot: React.FC = () => {
                 <span className="material-symbols-outlined text-3xl">
                     {isOpen ? 'close' : 'chat_bubble'}
                 </span>
-                {!isOpen && (
-                    <span className="absolute -top-1 -right-1 flex h-4 w-4">
-                        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-                        <span className="relative inline-flex rounded-full h-4 w-4 bg-green-500"></span>
-                    </span>
-                )}
             </motion.button>
         </div>
     );
