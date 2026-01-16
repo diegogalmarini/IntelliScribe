769d42532f944ff13e4945f2a5bec71508d91ac4
DiegoGalma <diegogalmarini@gmail.com>
2026-01-16 07:44:22 +0100

    fix: add trackEvent import only (v0.0.14)

--- pages/intelligence/IntelligenceDashboard.tsx
+++ pages/intelligence/IntelligenceDashboard.tsx
@@ -21,7 +21,6 @@ import { getSignedAudioUrl, uploadAudio } from '../../services/storageService';
 import { databaseService } from '../../services/databaseService';
 import { notifyNewRecording } from '../../services/emailService';
 import { useToast } from '../../components/Toast';
-import { concatenateAudios, timeToSeconds } from '../../services/audioConcat';
 
 interface IntelligenceDashboardProps {
     onNavigate: (route: AppRoute) => void;
